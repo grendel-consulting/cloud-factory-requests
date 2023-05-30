045c1c15e86c05e2ef985f76a6a60c51f5de662a
James Ramirez <james.ramirez@grendel-consulting.com>
2023-05-30 05:11:26 +0100

    feat: vend onibi site prod

---
+++ terraform/work-onibi-site-prod.tf
@@ -0,0 +1,27 @@
+module "work_onibi_site_prod_request" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "work-onibi-site-prod@grendel-consulting.com" # Google Groups, must be unique
+    AccountName               = "work-onibi-site-prod"
+    ManagedOrganizationalUnit = "Workloads"
+    SSOUserEmail              = "operations@grendel-consulting.com" # Default ops email
+    SSOUserFirstName          = "Operations"
+    SSOUserLastName           = "Controller"
+  }
+
+  account_tags = { # If Org tags being used
+    "Environment" = "Production"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "ramirezj"
+    change_reason       = "Production Onibi imprint website"
+  }
+
+  custom_fields = {
+    "control-plane" = var.CONTROL_PLANE_ACCOUNT_ID
+  } # If being used for customisation pipeline or IAM SSO, for example
+
+  account_customizations_name = "work-onibi-site" # Declare in customisation repo
+}
